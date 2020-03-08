d5fcc73966b9ffb26072e5aa5a02442318e5e6db
HidekiHrk <hidekihiroki123@gmail.com>
2020-03-08 20:04:00 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 function renderProfile(){
     let pageTitle = document.querySelector('head').querySelector('title');
     let propertyTitle = document.querySelector('head').querySelector('#propertyTitle');
+    let nick = document.querySelector('a#nick');
     let name = document.querySelector('a#name');
+
     let infoQueries = ['followers', 'following', 'public_repos', 'public_gists', 'location']
         .map(q => document.querySelector(`#${q}`));
     let userApiReq = new XMLHttpRequest();
@@ -12,8 +14,8 @@ function renderProfile(){
             // console.log(response);
             pageTitle.innerText = `${response.login}: Home`;
             propertyTitle.content = pageTitle.innerText;
-            name.innerHTML = response.name;
-            name.href = response.html_url;
+            nick.innerHTML = response.login;
+            nick.href = response.html_url;
             infoQueries.forEach(q => {
                 q.innerHTML = response[q.id];
             });

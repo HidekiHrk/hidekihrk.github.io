7a35feb1fa51f03c8f335583addf88f39a9e60ca
HidekiHrk <hidekihiroki123@gmail.com>
2020-06-17 02:12:58 -0300

    Updates

---
+++ precache-manifest.a01fc35d275adcfe6548ad35181eeb48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21a297662d3eb8ca1c145a56cf8ba73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e6120b17102b2e9a161",
+    "url": "/static/css/main.1143f13f.chunk.css"
+  },
+  {
+    "revision": "d11bb0eae59be50e164d",
+    "url": "/static/js/2.ba867d0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba867d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6120b17102b2e9a161",
+    "url": "/static/js/main.ebb443b0.chunk.js"
+  },
+  {
+    "revision": "5692d616658397ca4d3d",
+    "url": "/static/js/runtime-main.af95b56f.js"
+  },
+  {
+    "revision": "addbb673ce35a6f0a1e08b2757a93846",
+    "url": "/static/media/FiraCode.addbb673.ttf"
+  }
+]);

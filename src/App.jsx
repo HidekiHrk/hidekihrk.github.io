5df0d7bf0d8a7cdc2ab16c5d9aa8e6744c01ccc1
HidekiHrk <hidekihiroki123@gmail.com>
2020-06-17 01:51:13 -0300

    react program

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Router from "./routes";
+
+const App = () => (<div className="mainDiv">
+    <Router/>
+    <h1>SLA MAN TO COM SONO</h1>
+</div>);
+
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import Header from "./templates/header";
+
+const Router = () => (
+    <BrowserRouter>
+        <Header/>
+        <Switch>
+            <Route exact path="/">
+                <h1>Home</h1>
+            </Route>
+            <Route path="/projects/:id?">
+                <h1>Projects</h1>
+            </Route>
+            <Route path="/partners">
+                <h1>Partners</h1>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
